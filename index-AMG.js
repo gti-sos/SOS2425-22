573d8c5116557bd4b59b6b76155a0b85caccf596
Alejandro Morilla <alejandromorilla2001@gmail.com>
2025-02-21 19:17:38 +0100

    primer commit con el index-AMG casi acabado

---
+++ index-AMG.js
@@ -0,0 +1,78 @@
+let datos = [{
+    year:2023,
+    province:"Albacete",
+    missing_men:95,
+    missing_women:78,
+    missing_unknown:0,
+    total_population:368464
+},{
+    year:2023,
+    province:"Alicante",
+    missing_men:459,
+    missing_women:331,
+    missing_unknown:0,
+    total_population:1881762
+},{
+    year:2023,
+    province:"Albacete",
+    missing_men:95,
+    missing_women:78,
+    missing_unknown:0,
+    total_population:368464
+},{
+    year:2023,
+    province:"Albacete",
+    missing_men:95,
+    missing_women:78,
+    missing_unknown:0,
+    total_population:368464
+},{
+    year:2023,
+    province:"Albacete",
+    missing_men:95,
+    missing_women:78,
+    missing_unknown:0,
+    total_population:368464
+},{
+    year:2023,
+    province:"Albacete",
+    missing_men:95,
+    missing_women:78,
+    missing_unknown:0,
+    total_population:368464
+},{
+    year:2023,
+    province:"Albacete",
+    missing_men:95,
+    missing_women:78,
+    missing_unknown:0,
+    total_population:368464
+},{
+    year:2023,
+    province:"Albacete",
+    missing_men:95,
+    missing_women:78,
+    missing_unknown:0,
+    total_population:368464
+},{
+    year:2023,
+    province:"Albacete",
+    missing_men:95,
+    missing_women:78,
+    missing_unknown:0,
+    total_population:368464
+},{
+    year:2023,
+    province:"Albacete",
+    missing_men:95,
+    missing_women:78,
+    missing_unknown:0,
+    total_population:368464
+},{
+    year:2023,
+    province:"Albacete",
+    missing_men:95,
+    missing_women:78,
+    missing_unknown:0,
+    total_population:368464
+}];
