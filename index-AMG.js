0dae1141384da72c8b40b896b1b3b2492668d3cb
Alejandro Morilla <alejandromorilla2001@gmail.com>
2025-02-24 12:59:38 +0100

    finalizado F03

--- index-AMG.js
+++ index-AMG.js
@@ -14,65 +14,77 @@ let datos = [{
     total_population:1881762
 },{
     year:2023,
-    province:"Albacete",
-    missing_men:95,
-    missing_women:78,
+    province:"Almeria",
+    missing_men:164,
+    missing_women:97,
     missing_unknown:0,
-    total_population:368464
+    total_population:731792
 },{
     year:2023,
-    province:"Albacete",
-    missing_men:95,
-    missing_women:78,
+    province:"Araba",
+    missing_men:35,
+    missing_women:29,
     missing_unknown:0,
-    total_population:368464
+    total_population:333626
 },{
     year:2023,
-    province:"Albacete",
-    missing_men:95,
-    missing_women:78,
+    province:"Asturias",
+    missing_men:251,
+    missing_women:353,
     missing_unknown:0,
-    total_population:368464
+    total_population:1011792
 },{
     year:2023,
-    province:"Albacete",
-    missing_men:95,
-    missing_women:78,
+    province:"Avila",
+    missing_men:36,
+    missing_women:22,
     missing_unknown:0,
-    total_population:368464
+    total_population:158421
 },{
     year:2023,
-    province:"Albacete",
-    missing_men:95,
-    missing_women:78,
+    province:"Badajoz",
+    missing_men:47,
+    missing_women:73,
     missing_unknown:0,
-    total_population:368464
+    total_population:669943
 },{
     year:2023,
-    province:"Albacete",
-    missing_men:95,
-    missing_women:78,
+    province:"Balears",
+    missing_men:485,
+    missing_women:345,
     missing_unknown:0,
-    total_population:368464
+    total_population:1173008
 },{
     year:2023,
-    province:"Albacete",
-    missing_men:95,
-    missing_women:78,
+    province:"Barcelona",
+    missing_men:1462,
+    missing_women:993,
     missing_unknown:0,
-    total_population:368464
+    total_population:5714730
 },{
     year:2023,
-    province:"Albacete",
-    missing_men:95,
-    missing_women:78,
+    province:"Bizkaia",
+    missing_men:91,
+    missing_women:89,
     missing_unknown:0,
-    total_population:368464
+    total_population:1154334
 },{
     year:2023,
-    province:"Albacete",
-    missing_men:95,
-    missing_women:78,
+    province:"Burgos",
+    missing_men:66,
+    missing_women:38,
     missing_unknown:0,
-    total_population:368464
+    total_population:356055
 }];
+
+function mediaMissingMen(dat){
+    let ac = 0;
+    let lista = dat;
+    lista.filter(n=>n.province.startsWith("A")) 
+    .forEach(n => {
+        ac = ac + n.missing_men;
+    });
+    return ac/lista.length; 
+};
+
+console.log(mediaMissingMen(datos));
