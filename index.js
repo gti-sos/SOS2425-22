2024b2fc878fb34300a964ecc078f5750b7fa040
aaleejandro01 <alejandromorilla2001@gmail.com>
2025-03-06 14:10:18 +0100

    despliegue server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 16078;
+
+app.use("/",express.static("./public"));
+
+app.get("/hello",(request,response) => {
+    response.send("Hello from the server!");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server running on ${PORT}!`);
+});
