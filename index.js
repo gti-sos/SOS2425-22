4de19d068f211e3ed4b3a353e457cb021506f7ed
juariolop1 <juariolop1@gmail.com>
2025-04-20 17:16:41 +0200

    prueba ruta handler

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 16078;
 import cors from "cors";
 import { loadBackendAMG } from './src/backend/index-AMG.js';
 import { loadBackendJMRL } from './src/backend/index-JMRL.js';
-import { handler } from 'src/frontend/build/handler.js'
+import { handler } from './src/frontend/build/handler.js'
 
 app.use(express.json());
 app.use(cors());
